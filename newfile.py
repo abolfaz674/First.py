4be5c82776f10094f35af9c4bf191d8d8d72bd9d
abolfaz674 <moslehiseyedabolfazl@gmail.com>
2025-04-12 10:30:12 +0330

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,9 +1,14 @@
-
-print (1 + 8 )
-print (4 * 9 )
-print (81 / 9 )
-print (12 - 10 )
-print (3 + 8 * 12 )
-print (12 ** 6 )
-print (89 % 12 )
-print (86 // 5)
+a = 4
+b = 2
+c = a+b
+print (c)
+d = a-b
+print (d)
+e = a*b
+print (e)
+f = a**b
+print (f)
+g = a/b
+print (g)
+h = a//b
+print (h)
